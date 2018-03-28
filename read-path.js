60b739cf32768ac0afece226c98c40b285c34972
Patrick Allen <allenpatrick86@gmail.com>
2018-03-28 14:46:50 -0700

    Complete basic refactor, add npm framework, start building for tests

--- read-path.js
+++ read-path.js
@@ -17,7 +17,7 @@ const yetMorePath = [ 'terr' ]
 
 // Can we construct what a path is differently? How can we check?
 
-// "type" can be either single, optional, one-of-many
+// "type" can be either single, one-of-many
 // "paths" will be handled differently for different types
 // Consider a "name" category as well
 const pathObject = {
@@ -37,9 +37,13 @@ const oneOfManyTest = {
   paths: [ [ 'bah', 'someKey' ], badPath ],
   name: 'internalFieldName3'
 }
+
 // ----------------------------------------------------------
-// ----- Deprecated Functionality
+// ----- Actual Functionality
 // ----------------------------------------------------------
+function errorDetailsOrNull (arr, details) {
+  return arr.length > 0 ? details : null
+}
 
 // This FN Will work correctly for only 1 state of an object
 function verifyObjectShape (paths, obj) {
@@ -54,29 +58,27 @@ function verifyObjectShape (paths, obj) {
   return pathFailures
 }
 
-// ----------------------------------------------------------
-// ----- Actual Functionality
-// ----------------------------------------------------------
-function populatedArrayOrNull (arr) {
-  return arr.length > 0 ? arr : null
-}
 
 function verifySinglePath (pathContainer, obj) {
-  // If we got an empty path, return empty result as it technically passes
   if (pathContainer.length === 0) return []
-  // If we got an undefined result, return the path as a failure case
-  if (R.path(pathContainer[0], obj) === undefined) return pathContainer[0]
-  // Otherwise, return empty result as the path exists
+
+  const pathResult = R.path(pathContainer[0], obj)
+
+  if (pathResult === undefined) return pathContainer[0]
+
   return []
 }
 
 function verifyMultiPath (pathContainers, obj) {
   const pathResults = pathContainers.reduce((badPaths, path) => {
+    const pathResult = R.path(path, obj)
+
     // If Ramda returns an undefined for a path, it isn't there
-    if (R.path(path, obj) === undefined) {
+    if (pathResult === undefined) {
       badPaths.push(path)
       return badPaths
     }
+
     // Otherwise, verify one of the requested paths is present
     badPaths.push(true)
     return badPaths
@@ -92,19 +94,18 @@ function verifyMultiPath (pathContainers, obj) {
 // Otherwise, return null or true or something
 function determineStrategy (obj, fieldDetails) {
   if (fieldDetails.type === 'single') {
-    const verificationResult = verifySinglePath(fieldDetails.paths, obj)
-    console.log('Verification Result: ', verificationResult)
-
-    return populatedArrayOrNull(verificationResult)
+    return errorDetailsOrNull(
+      verifySinglePath(fieldDetails.paths, obj),
+      fieldDetails
+    )
   }
 
   if (fieldDetails.type === 'one-of-many') {
-    const verificationResult =  verifyMultiPath(fieldDetails.paths, obj)
-
-    return populatedArrayOrNull(verificationResult)
+    return errorDetailsOrNull(
+      verifyMultiPath(fieldDetails.paths, obj),
+      fieldDetails
+    )
   }
-
-  if (fieldDetails.type === 'optional') {}
 
   // Could throw
   console.error('Error: We received an unknown field type')
@@ -118,5 +119,24 @@ function reviewObjectStructure (fieldList, obj) {
     .filter(arr => arr != null)
 }
 
-reviewObjectStructure([pathObject, badPathObject, oneOfManyTest], testObject)
+function processErrorReturns (verificationArr) {
+  if (verificationArr.length === 0) return null
 
+  return verificationArr.forEach((failure) => {
+    const { paths, name, type } = failure
+    const fieldError = `The following Path(s) failed: `
+    const generalError = `Error when verifying Field: ${name} (${type}) \n${fieldError}`
+
+    console.error(generalError, paths)
+  })
+}
+
+function verifyObjectPaths (pathObjects, objects) {
+  return processErrorReturns(
+    reviewObjectStructure([pathObject, badPathObject, oneOfManyTest], testObject)
+  )
+}
+
+const verification = reviewObjectStructure([pathObject, badPathObject, oneOfManyTest], testObject)
+
+processErrorReturns(verification)
